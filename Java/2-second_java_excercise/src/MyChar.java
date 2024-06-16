8e18fabf440635965381e03fd2ac01ab51364318
collich <collin-chua@hotmail.com>
2024-06-17 00:05:54 +0800

    Added Java code for MyChar Class

---
+++ Java/2-second_java_excercise/src/MyChar.java
@@ -0,0 +1,56 @@
+public class MyChar {
+    private char character;
+
+    public MyChar(char character){
+        this.character = character;
+    }
+
+    public boolean isVowel(){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (int i = 0; i < vowels.length; i++) {
+            if (this.character == vowels[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean isNumber(){
+        if (this.character >= 48 && this.character <= 57){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isAlphabet(){
+        if (this.character >= 65 && this.character <= 90){
+            return true;
+        }
+        if (this.character >= 97 && this.character <= 122){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isConsonant(){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (int i = 0; i < vowels.length; i++) {
+            if (isAlphabet() && !isVowel()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printAllLowerChar(){
+        for (char ch = 'a'; i <= 'z'; ch++) {
+            System.out.println(ch);
+        }
+    }
+
+    public void printAllUpperChar(){
+        for (char ch = 'A'; i <= 'Z'; ch++) {
+            System.out.println(ch);
+        }
+    }
+}
--- Java/App.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-
-public class App {
-    public static void main(String[] args) {
-        SimpleInterestFormula calculator = new SimpleInterestFormula("4500.00", "7.5");
-        BigDecimal totalValue = calculator.calculateTotalValue(5);
-        System.out.println(totalValue);
-    }
-}
