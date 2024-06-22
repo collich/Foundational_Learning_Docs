d90acf042343a3cd40e9feb96a8fc6b4f585cbe3
collich <collin-chua@hotmail.com>
2024-06-22 23:25:57 +0800

    Added a Student Class

---
+++ Java/4-forth_excercise/Student.java
@@ -0,0 +1,47 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Student {
+    private String name;
+    private int[] listOfMarks = {};
+
+    public Student(String name, int[] listOfMarks){
+        this.name = name;
+        this.listOfMarks = listOfMarks;
+    }
+
+    public int getNumberOfMarks(){
+        return this.listOfMarks.length;
+    }
+
+    public int getTotalSumOfMarks(){
+        int total = 0;
+        for (int number : listOfMarks){
+            total += number;
+        }
+        return total;
+    }
+
+    public int getMaximumMark(){
+        int max = Integer.MIN_VALUE;
+        for (int number: this.listOfMarks){
+            max = Integer.max(max, number);
+        }
+        return max;
+    }
+
+    public int getMinimumMark(){
+        int min = Integer.MAX_VALUE;
+        for (int number: this.listOfMarks){
+            min = Integer.min(min, number);
+        }
+        return min;
+    }
+
+    public BigDecimal getAverageMarks(){
+        BigDecimal total = new BigDecimal(getTotalSumOfMarks());
+        BigDecimal number = new BigDecimal(getNumberOfMarks());
+
+        return total.divide(number, 3, RoundingMode.UP);
+    }
+}
--- Java/5-Array_&_ArrayList.md
+++ Java/5-Array_&_ArrayList.md
@@ -14,8 +14,22 @@ listInt.length();
 // We can also use Reference Classes
 System.out.println(Array.toString(listInt));
 
-// We can use an enhanced for loop as well.
+// We can use an `enhanced for loop` as well.
 for (int number : listInt){
     System.out.print(number);
 }
+```
+
+There are a few useful methods that we can use.
+
+```Java
+// fill in an empty Array
+int[] listArray = new int[5];
+
+Arrays.fill(listArray, 100);
+
+// Check for equality with another Array
+int[] listArray1 = {100, 100, 100, 100, 100};
+
+Array.equals(listArray, listArray1);
 ```
