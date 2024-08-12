f5bde1e02438e0f2e53a9567f206db51dbb89b5f
collich <collin-chua@hotmail.com>
2024-08-13 00:09:11 +0800

    Added content for Adding View Resolver to the Controller

--- Spring/5-Spring_Boot_WebApp/myfirstwebapp/src/main/java/com/collich/springboot/myfirstwebapp/hello/SayHelloController.java
+++ Spring/5-Spring_Boot_WebApp/myfirstwebapp/src/main/java/com/collich/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @RequestMapping
 public class SayHelloController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String sayHello() {
+    public String sayHelloWorld() {
         return "Hello World!";
     }
+
+    @GetMapping("hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
+    
 }
