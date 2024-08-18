3611109e0feb26723d24cf06e005858d27efc9c6
collich <collin-chua@hotmail.com>
2024-08-18 23:09:07 +0800

    Added Request Params section and content

--- Spring/5-Spring_Boot_WebApp/myfirstwebapp/src/main/java/com/collich/springboot/myfirstwebapp/login/LoginController.java
+++ Spring/5-Spring_Boot_WebApp/myfirstwebapp/src/main/java/com/collich/springboot/myfirstwebapp/login/LoginController.java
@@ -3,13 +3,15 @@ package com.collich.springboot.myfirstwebapp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 public class LoginController {
 
     @GetMapping("login")
-    public String loginPage(){
+    public String loginPage(@RequestParam String name){
+        System.out.println(name);
         return "login";
     }
 }
